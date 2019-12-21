d2b8ecde814116b7bef76c2379a8eb87991c1f17
40149527 <ibhuiyan01@qub.ac.uk>
2019-12-21 20:03:25 +0000

    set up react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 //import './App.css';
 import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
 
+import Navbar from './components/Navbar'
+
 function App() {
   return (
-    <div className="App">
-      <p>Hello from app.</p>
-      <Home></Home>
-      <Rooms></Rooms>
-      <SingleRoom></SingleRoom>
-      <Error></Error>
+    <div>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/rooms" component={Rooms}/>
+        <Route exact path="/rooms/:slug" component={SingleRoom}/>
+        <Route component={Error}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        I'm a Navbar.
+      </div>
+    )
+  }
+}
